76316d4a6ef0c4a0a43d2354d9dec6c4be5f1f4f
hzshang <hzshang15@gmail.com>
2017-06-21 22:36:47 +0800

    factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,18 @@
+//http://www.spoj.com/problems/FCTRL/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int N,sum,num,mul;
+	cin>>N;
+	while(N--){
+		cin>>num;
+		mul=5;
+		sum=0;
+		while(mul<=num){
+			sum+=num/mul;
+			mul*=5;
+		}
+		cout<<sum<<endl;
+	}
+}
